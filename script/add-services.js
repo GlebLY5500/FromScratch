a074af7683bf9a0aa9e5a56e804db72cd02d012d
GlebLY5500 <lyolisgleb@gmail.com>
2026-04-29 19:02:17 +0300

    Vers.2.0.0

---
+++ script/add-services.js
@@ -0,0 +1,67 @@
+
+const USD_TO_BYN = 3.25; 
+
+async function loadServices() {
+    try {
+        const response = await fetch('data/services.xml');
+        const xmlText = await response.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+        const nodes = xmlDoc.getElementsByTagName("service");
+
+        allServices = Array.from(nodes).map(s => {
+            const val = s.getElementsByTagName("price")[0].textContent;
+            const numericPrice = parseFloat(val) || 0;
+            
+            return {
+                title: s.getElementsByTagName("title")[0].textContent,
+                desc: s.getElementsByTagName("description")[0].textContent,
+                priceBYN: numericPrice,
+                cat: s.getElementsByTagName("category")[0].textContent
+            };
+        });
+
+        renderServices(allServices);
+    } catch (error) {
+        console.error("Ошибка:", error);
+    }
+}
+
+function renderServices(data) {
+    const grid = document.getElementById("services-grid");
+    grid.innerHTML = '';
+
+    data.forEach(s => {
+        let priceHTML = '';
+
+        if (s.priceBYN > 0) {
+            const usd = (s.priceBYN / USD_TO_BYN).toFixed(0);
+            
+            priceHTML = `
+                <div class="main-price">${s.priceBYN} BYN</div>
+                <div class="alt-price">\u{2248}${usd}$</div>
+            `;
+        } else {
+            priceHTML = '<div class="main-price">Индивидуальный расчет</div>';
+        }
+
+        grid.innerHTML += `
+            <div class="service-card">
+                <span class="service-card__category">${s.cat}</span>
+                <h3 class="service-card__name">${s.title}</h3>
+                <p class="service-card__text">${s.desc}</p>
+                <div class="service-card__price">${priceHTML}</div>
+            </div>
+        `;
+    });
+}
+
+
+function sortServices(type) {
+    let sorted = [...allServices];
+    if (type === 'asc') sorted.sort((a, b) => a.priceBYN - b.priceBYN);
+    else if (type === 'desc') sorted.sort((a, b) => b.priceBYN - a.priceBYN);
+    renderServices(sorted);
+}
+
+document.addEventListener("DOMContentLoaded", loadServices);
